ed3a2274b1d837e580c8476bb36f9790246c2c45
feraariska <96222163+feraariska@users.noreply.github.com>
2022-01-08 15:21:08 +0700

    Create activity_main.java

---
+++ activity_main.java
@@ -0,0 +1,56 @@
+package com.example.uassensor;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.Service;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class uassensor extends Activity implements SensorEventListener{
+    //SensorManager lets you access the device's sensors
+    //declare Variables
+    TextView textView,TVAirPressure;
+    private SensorManager sensorManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView=(TextView)findViewById(R.id.TextView);
+        TVAirPressure=(TextView)findViewById(R.id.TVAirPressure);
+        //create instance of sensor manager and get system service to interact with Sensor
+        sensorManager = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // register this class as a listener for the Pressure Sensor
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    // called when sensor value have changed
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_PRESSURE) {
+            float[] values = event.values;
+            TVAirPressure.setText("" + values[0]);
+        }
+
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        // unregister listener
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+}
